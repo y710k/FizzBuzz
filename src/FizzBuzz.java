ad46ce9d246035e2abe545b4f15b03df9821aeac
InoueHiroki <180195602+y710k@users.noreply.github.com>
2024-09-10 00:53:20 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,4 +1,20 @@
 
 public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int num = 1; num <= 100; num++) {
+			if (num % 15 == 0) {
+				System.out.println("FizzBuzz");
+			
+			} else if (num % 3 == 0) {
+				System.out.println("Fizz");
+			
+			} else if (num % 5 == 0) {
+				System.out.println("Buzz");
+			
+			} else {
+				System.out.println(num);
+			}
+		}
 
+	}
 }
